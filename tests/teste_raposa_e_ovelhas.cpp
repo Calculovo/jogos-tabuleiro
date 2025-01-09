4758404f2da4c13e50d9e2010c1c634ceb4a8360
pedroluckz <pedrolgcalais@gmail.com>
2025-01-09 14:49:33 -0300

    Adicionado o teste da Raposa e Ovelhas

---
+++ tests/teste_raposa_e_ovelhas.cpp
@@ -0,0 +1,21 @@
+#include <raposaeovelhas.hpp>
+#include <iostream>
+using namespace std;
+
+int main() {
+    RaposaeOvelhas j;
+    string entrada;
+    while (j.testarVitoria() == INDEFINIDO) {
+        j.imprimirTabuleiro();
+        cout << "Vez do jogador ";
+        cout << (j.getTurno() == PLAYER_1 ? "1" : "2") << ":" << endl;
+        getline(cin, entrada);
+        if (entrada == "Y"){
+            return 0;
+        }
+        j.validarJogada(entrada);
+    }
+    j.imprimirTabuleiro();
+    cout << "Player " << (j.testarVitoria() == PLAYER_1 ? "1" : "2") << " ganhou!" << endl;
+    return 0;
+}
