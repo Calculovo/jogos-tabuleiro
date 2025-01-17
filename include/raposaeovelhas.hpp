305cdf75838ea9cd1653261b48be61224c9bc34d
Calculovo <calculovolinguistics@gmail.com>
2025-01-17 12:57:02 -0300

    Adicionado comentarios em raposaeovelhas.hpp

--- include/raposaeovelhas.hpp
+++ include/raposaeovelhas.hpp
@@ -5,13 +5,17 @@
 
 class RaposaeOvelhas : public JogoBase {
     public:
+        //! Construtor.
         RaposaeOvelhas();
         char validarJogada(std::string input) override;
         int testarVitoria() override;
         void imprimirTabuleiro() override;
     private:
+        //! Move a raposa da origem para o destino.
         bool moverRaposa(Coord origem, Coord destino);
+        //! Move uma ovelha da origem para o destino.
         bool moverOvelha(Coord origem, Coord destino);
+        //! Verifica se um movimento da origem para o destino é válido.
         bool movimentoValido(Coord origem, Coord destino);
 };
 #endif
