5cac815bfeaf428e4c932dbd94cfd8b66a1c769f
Calculovo <calculovolinguistics@gmail.com>
2025-01-17 12:44:35 -0300

    Adicionado comentários em partida.hpp, apeasar de não fazer muito sentido.

--- include/partida.hpp
+++ include/partida.hpp
@@ -6,11 +6,16 @@
 
 class Partida {
     private:
+        //! Ponteiro para o jogo.
         JogoBase* jogo;
+        //! Ponteiro para o primeiro jogador.
         Jogador* jogador1;
+        //! Ponteiro para o segundo jogador.
         Jogador* jogador2;
     public:
+        //! Inicializa uma partida.
         Partida();
+        //! ???
         void jogar();
 };
 
