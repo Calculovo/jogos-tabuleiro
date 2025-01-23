fa891dea2df6dc79c0ef3488f08840cf0a73484f
Calculovo <calculovolinguistics@gmail.com>
2025-01-23 18:03:16 -0300

    Revert "Adicionando informações para que o código funcione bem com o doxygen."
    This reverts commit 5ad8fa35ab3a1d4140f44db7198664cf9f47b8d1.

---
+++ include/cadastro.hpp
@@ -0,0 +1,17 @@
+#ifndef CADASTRO_I
+#define CADASTRO_I 1
+
+#include <vector>
+#include "jogador.hpp"
+
+class Cadastro {
+    private:
+        std::vector<Jogador> jogadores;
+    public:
+        bool cadastrarJogador(std::string apelido, std::string nome);
+        bool removerJogador(std::string apelido);
+        bool listarJogadoresApelido();
+        bool listarJogadoresNome();
+};
+
+#endif
