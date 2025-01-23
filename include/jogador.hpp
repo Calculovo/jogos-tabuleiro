dac4cb0935e86cb1d6a2682b6247189ffc720332
Calculovo <calculovolinguistics@gmail.com>
2025-01-23 18:05:11 -0300

    Revert "Adicionados comentários em jogador.hpp"
    This reverts commit 81a5ac2d711228a1eb868d64962eb1e998a1cef3.

--- include/jogador.hpp
+++ include/jogador.hpp
@@ -19,26 +19,17 @@ using namespace std;
 
 class Jogador {
     private:
-        //! Visualmente reconhece um jogador.
         string nome;
-        //! Identifica unicamente um jogador. Não pode ser repetido.
         string apelido;
-        //! Armazena vitorias, empates e derrotas em cada um dos cinco jogos.
         int resultados[N_DE_JOGOS][N_DE_RESULTADOS];
     public:
-        //! Construtor básico, que inicializa as estatísticas para zero.
         Jogador(string nome, string apelido);
-        //! Construtor mais complexo, que inicializa as estatísticas para outros valores.
         Jogador(string nome, string apelido, int valores[5][3]);
-        //! Adiciona uma vitória, empate, ou derrota em um jogo.
-        int addResultados(int jogo, int resultado);
-        //! Retorna o número de vitórias, empates, ou derrotas em um jogo.
+        int AddResultados(int jogo, int resultado);
+        
         int getResultados(int jogo, int resultado) const;
-        //! Imprime para o cout as informações do jogador.
         void imprimir() const;
-        //! Retorna o nome do jogador.
         string getNome() const;
-        //! Retorna o apelido do jogador.
         string getApelido() const;
 };
 
