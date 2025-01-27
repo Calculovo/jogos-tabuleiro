31219427fb2cde81e57959656329a38292410efd
zuzu <emerson.arauujo77@gmail.com>
2025-01-27 20:28:13 -0300

    Adicionados comentários no parida.hpp

--- include/partida.hpp
+++ include/partida.hpp
@@ -12,12 +12,15 @@
 
 class Partida {
     private:
+    //! Recebe o comando correspondente ao jogo e dois ponteiros de jogadores, criando um spart pointer para o jogo selecionado.
         unique_ptr<JogoBase> jogo;
         Jogador* jogador1;
         Jogador* jogador2;
         char comandoJogo;
     public:
+    //! Construtor da partida. Cria o jogo selecionado com os jogadores passados no construtor.
         Partida(char comandoJogo, Jogador* jogador1, Jogador* jogador2);
+    //! Imprime o tabuleiro e troca os turnos até que uma condição de vitória para o jogo seja alcançada.
         void jogar();
 };
 
