b1568c5b973bf8fb8caa361a7635ee4f8ce06400
zuzu <emerson.arauujo77@gmail.com>
2025-01-27 20:28:57 -0300

    Ifs substutuidos por switch case no partida.cpp

--- src/partida.cpp
+++ src/partida.cpp
@@ -20,7 +20,7 @@ Partida::Partida(char comandoJogo, Jogador* jogador1, Jogador* jogador2) : jogad
             jogo = std::make_unique<Damas>();
             break;
         default:
-            throw invalid_argument("Jogo nao reconhecido.");
+            throw invalid_argument("Jogo não reconhecido.");
             break;
     }
 };
@@ -36,20 +36,23 @@ void Partida::jogar() {
         jogo->validarJogada(input);
     }
     jogo->imprimirTabuleiro();
-    if (jogo->testarVitoria() == EMPATE) {
+    switch (jogo->testarVitoria()) {
+    case EMPATE:
         std::cout << "Empate." << std::endl;
         jogador1->addResultados(comandoJogo, EMPATE_INDICE);
         jogador2->addResultados(comandoJogo, EMPATE_INDICE);
-        return;
-    }
-    else if (jogo->testarVitoria() == PLAYER_1) {
+        break;
+    case PLAYER_1:
         std::cout << "Jogador 1 ganhou!" << std::endl;
         jogador1->addResultados(comandoJogo, VITORIA_INDICE);
         jogador2->addResultados(comandoJogo, DERROTA_INDICE);
-    }
-    else if (jogo->testarVitoria() == PLAYER_2) {
+        break;
+    case PLAYER_2:
         std::cout << "Jogador 2 ganhou!" << std::endl;
         jogador2->addResultados(comandoJogo, VITORIA_INDICE);
         jogador1->addResultados(comandoJogo, DERROTA_INDICE);
+        break;
+    default:
+        break;
     }
 }
